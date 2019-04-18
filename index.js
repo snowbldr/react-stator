735fa1b435a0b28dcab8e227d61d3d19a8a81fc4
Robert Kempton <r@snow87.com>
2019-04-17 20:36:13 -0600

    Export objectpaths too so it can be used elsewhere

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import {stateful, StatefulComponent} from './src/StatefulComponent'
 import SharedStateProvider from './src/SharedStateProvider'
-
+import ObjectPaths from './src/ObjectPaths'
 
 export {
-    stateful, SharedStateProvider, StatefulComponent
+    stateful, SharedStateProvider, StatefulComponent, ObjectPaths
 }
